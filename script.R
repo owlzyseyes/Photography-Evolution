cdc8262d12a8135c58a1b3b3b984829dfb1c3b5d
Brian <monsieursapien@gmail.com>
2025-05-02 13:42:36 +0300

    Updated .gitignore to exclude yearly photo folders

--- script.R
+++ script.R
@@ -4,14 +4,13 @@ library(dplyr)
 library(lubridate)
 
 # Read the image
-img <- image_read("2022_photos/20221021_142227.jpg")
+img <- image_read("2022_photos/20221127_161729.jpg")
 
 # Normalize to enhance contrast and colour separation
 img <- image_normalize(img)
 
 # Reduce to n dominant colors (3 is my arbitrary choice)
 dominant_colors <- image_quantize(img, max = 3)
-print(dominant_colors)
 
 # Convert to raw data
 raw_colors <- image_data(dominant_colors)
@@ -23,11 +22,10 @@ hex_codes <- unique(apply(raw_colors, 2, function(rgb)
 }))
 print(hex_codes)
 
-
 # Get the image metadata
-metadata <- read_exif("2022_photos/20221021_142227.jpg")
+metadata <- read_exif("2022_photos/20221127_161729.jpg")
 # Get the DateTimeOriginal 
-datetime <- metadata$DateTimeOriginal
+datetime <- metadata$CreateDate
 print(datetime)
 
 
